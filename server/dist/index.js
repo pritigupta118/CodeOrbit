751612d2bc41fda0134083c50eaf8011d90a469a
Priti Gupta <civilstudent9900@gmail.com>
2024-12-04 14:58:41 +0530

    little fix

--- server/dist/index.js
+++ server/dist/index.js
@@ -14,7 +14,7 @@ const app = (0, express_1.default)();
 (0, dotenv_1.config)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ credentials: true, origin: "http://localhost:5173" }));
+app.use((0, cors_1.default)({ credentials: true, origin: ["http://localhost:5173", process.env.CLIENT_URL], }));
 (0, dbConnect_1.dbConnect)();
 app.use("/compiler", compilerRoutes_1.compilerRouter);
 app.use("/user", userRoutes_1.userRouter);
