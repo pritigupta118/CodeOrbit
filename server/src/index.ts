03d11101e2f977fb3f23e9c4d0b79936aeba3016
Priti Gupta <civilstudent9900@gmail.com>
2024-12-04 14:58:07 +0530

    "Updated CORS origin to include CLIENT_URL environment variable"

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ config()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+app.use(cors({credentials: true,  origin: ["http://localhost:5173", process.env.CLIENT_URL!],}))
 
 dbConnect()
 
