eb31d14ec17f603b5babe95fdb334949f60a122c
Priti Gupta <civilstudent9900@gmail.com>
2024-12-05 12:54:12 +0530

    "Update navigation path in Login and Signup pages to '/compiler/:urlId?' from '/compiler'"

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -42,7 +42,7 @@ const Signup = () => {
     const response = await signupUser(values).unwrap()
     dispatch(updateCurrentUser(response))
     dispatch(updateIsLoggedIn(true))
-    navigate("/compiler")
+    navigate("/compiler/:urlId?")
   }
 
   return (
